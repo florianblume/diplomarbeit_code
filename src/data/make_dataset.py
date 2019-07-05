Continued implementing average network. Corrected small errors.

src/data/make_dataset.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import sys
 import shutil
+import skimage
 from skimage import io
 
 main_path = os.getcwd()
@@ -22,7 +23,7 @@ Script to create the datasets as they were used in the experiments.
 """
 
 ### Remove old data
-"""
+
 print('Removing old generated data.')
 shutil.rmtree(os.path.join(main_path, 'data/processed'))
 
@@ -159,54 +160,41 @@ util.merge_two_npy_datasets('data/processed/fish/gauss30',
                             'data/processed/mouse/gauss30',
                             'data/processed/joined/fish_mouse/gauss30_gauss30/')
 
-"""
+
 print('Processing SimSim dataset...')
 
-raw = io.imread('data/raw/simsim/camsim_ccd_phot300_rn8_bgrd0.tif')
-gt = io.imread('data/raw/simsim/noise_free_32b.tif')
-assert raw.shape[0] == gt.shape[0]
+train_raw = io.imread('data/raw/simsim/camsim_ccd_phot300_rn8_bgrd0.tif')
+train_gt = io.imread('data/raw/simsim/noise_free_32b.tif')
+assert train_raw.shape[0] == train_raw.shape[0]
 
-factor = int(raw.shape[0] / 3)
-
-train_raws = []
-train_gts = []
-
-for i in range(3):
-    train_raws.append(raw[i * factor:(i + 1) * factor])
-    train_gts.append(gt[i * factor:(i + 1) * factor])
-
-# NOTE this is the actual test set, not the validation set
-test_size = 10
-
-test_raws = []
-test_gts = []
-
-from skimage import io
-
-# Take away test_size many images from raws and gts and create the test sets
-for i in range(3):
-    indices = np.random.choice(train_raws[i].shape[0], test_size, replace=False)
-    test_raws.append(train_raws[i][indices])
-    train_raws[i] = np.delete(train_raws[i], indices, axis=0)
-    test_gts.append(train_gts[i][indices])
-    train_gts[i] = np.delete(train_gts[i], indices, axis=0)
+factor = int(train_raw.shape[0] / 3)
 
 os.makedirs('data/processed/simsim/raw/')
 os.makedirs('data/processed/simsim/gt/')
-os.makedirs('data/processed/joined/simsim/')
+os.makedirs('data/processed/joined/simsim/raw')
+os.makedirs('data/processed/joined/simsim/gt')
 
 for i in range(3):
-    np.save('data/processed/simsim/raw/train_part_{}.npy'.format(str(i)), raw[i * factor:(i + 1) * factor])
-    np.save('data/processed/simsim/gt/train_part_{}.npy'.format(str(i)), gt[i * factor:(i + 1) * factor])
+    np.save('data/processed/simsim/raw/train_part_{}.npy'.format(str(i)), train_raw[i * factor:(i + 1) * factor])
+    np.save('data/processed/simsim/gt/train_part_{}.npy'.format(str(i)), train_gt[i * factor:(i + 1) * factor])
 
-train_raw = np.concatenate(train_raws, axis=0)
-test_raw = np.concatenate(test_raws, axis=0)
-np.save('data/processed/joined/simsim/train_raw.npy', train_raw)
-np.save('data/processed/joined/simsim/test_raw.npy', test_raw)
+np.save('data/processed/joined/simsim/raw/train_raw.npy', train_raw)
+np.save('data/processed/joined/simsim/gt/train_gt.npy', train_gt)
 
-train_gt = np.concatenate(train_gts, axis=0)
-test_gt = np.concatenate(test_gts, axis=0)
-np.save('data/processed/joined/simsim/train_gt.npy', train_gt)
-np.save('data/processed/joined/simsim/test_gt.npy', test_gt)
+# Create the test set
+
+test_raw = []
+test_gt = []
+
+for i in range(train_raw.shape[0]):
+    test_raw.append(skimage.transform.rotate(train_raw[i], 180))
+    test_gt.append(skimage.transform.rotate(train_gt[i], 180))
+
+for i in range(3):
+    np.save('data/processed/simsim/raw/test_part_{}.npy'.format(str(i)), test_raw[i * factor:(i + 1) * factor])
+    np.save('data/processed/simsim/gt/test_part_{}.npy'.format(str(i)), test_gt[i * factor:(i + 1) * factor])
+
+np.save('data/processed/joined/simsim/raw/test_raw.npy', test_raw)
+np.save('data/processed/joined/simsim/gt/test_gt.npy', test_gt)
 
 print('Done.')

src/models/average/image_weight_network.py
@@ -216,7 +216,10 @@ class UNet(nn.Module):
         self.final_op = nn.ModuleList()
 
         for i in range(self.num_subnets):
+            # We create each requested subnet
             self.subnets.append(SubUNet(num_classes, mean, std, depth=sub_net_depth))
+            # And we output a weight for each subnet based on the whole image
+            # That's why we employ torch.sum
             self.final_op.append(torch.sum(conv1x1(outs, self.num_classes)))
 
 
@@ -296,24 +299,33 @@ class UNet(nn.Module):
             self.device), labels.to(self.device), masks.to(self.device)
 
         # Forward step
-        # [batch, subnets, H, W]
+        # Actually we compute the weights for only one patch here, not the whole
+        # image. We could compute the weights for one image if we ran the network
+        # on all images that are fused in the batch that has been created above.
+        # This could fail if the images are too large. Also, we would have to
+        # obtain the indices of the images used in the batch to retrieve them.
+        # We assume that training the network on patches and during prediction
+        # averaging the weights works just as well since the network performs
+        # some kind of averaging already but only on the patch-level.
+        # [batch, subnets]
         weights = self(inputs)
         # [subnets, batch, H, W]
         sub_outputs = [sub(inputs) for sub in self.subnets]
         # [batch, subnets, H, W]
         sub_outputs = np.transpose(sub_outputs, axes=(1, 0, 2, 3))
-
+        # All pixels in each sub_output get multiplied by the same weight
+        # sum([batch, subnets] x [batch, subnets, H, W]) = [batch, H, W]
         outputs = np.sum(weights * sub_outputs, axis=1)
-        
+        # [batch, H, W] / [batch]
         outputs /= np.sum(weights, axis=1)
         
         return sub_outputs, weights, labels, masks, data_counter
 
     def predict(self, image, patch_size, overlap):
-        # pixel weighted average
-        weighted_average = np.zeros(image.shape)
+        # weights for each subnet for the whole imagess
+        weights = []
         # sub_images = [subnets, H, W]
-        sub_images = np.zeros((self.subnets.shape[0], image.shape))
+        sub_images = np.zeros((self.num_subnets, image.shape))
         # We have to use tiling because of memory constraints on the GPU
         xmin = 0
         ymin = 0
@@ -324,12 +336,10 @@ class UNet(nn.Module):
             ovTop = 0
             while (ymin < image.shape[0]):
                 patch = image[ymin:ymax, xmin:xmax]
-                weights = self.predict_patch(patch)
+                # We save all weights for all patches and average later
+                weights.append(self.predict_patch(patch))
                 sub_images[:, ymin:ymax, xmin:xmax][ovTop:, ovLeft:] 
                     = [subnet.predict_patch(patch) for subnet in self.subnets][:, ovTop:, ovLeft:] 
-                weighted_average[ymin:ymax, xmin:xmax][ovTop:, ovLeft:] = 
-                    np.sum(weights[ovTop:, ovLeft:] * predicted_images[ovTop:, ovLeft:], axis=0) / 
-                                            np.sum(weights, axis=0)
                 ymin = ymin-overlap+patch_size
                 ymax = ymin+patch_size
                 ovTop = overlap//2
@@ -339,7 +349,14 @@ class UNet(nn.Module):
             xmax = xmin+patch_size
             ovLeft = overlap//2
 
-        return weighted_average
+        # [image.shape[0] x image.shape[1] = num_patches, num_subnets]
+        weights = np.array(weights)
+        # [num_subnets] = weights for the whole image for each subnet
+        weights = np.mean(weights, axis=0)
+        # sub_images * weights = [num_subnets, H, W] * [num_subnets]
+        weighted_average = np.sum(sub_images * weights, axis=0) / np.sum(weights)
+
+        return weighted_average, weights
 
     def predict_patch(self, patch):
         """Performs network prediction on a patch of an image using the
@@ -363,17 +380,15 @@ class UNet(nn.Module):
 
         output = self(inputs)
 
-        samples = (output).permute(1, 0, 2, 3)
+        # In contrast to the other networks we only have a single output number
+        # per patch and not an output of shape [H, W] that's why we need to
+        # write (1, 0, 2) instead of (1, 0, 2, 3)
+        samples = (output).permute(1, 0, 2)
 
         # In contrast to probabilistic N2V we only have one sample
-        means = samples[0, ...]
+        weights = samples[0, ...]
 
         # Get data from GPU
-        means = means.cpu().detach().numpy()
+        weights = weights.cpu().detach().numpy()
 
-        # Reshape to 2D images and remove padding
-        means.shape = (output.shape[2], output.shape[3])
-
-        # Denormalize
-        means = util.denormalize(means, self.mean, self.std)
-        return means
+        return weights

src/models/average/pixel_weight_network.py
@@ -216,7 +216,9 @@ class UNet(nn.Module):
         self.final_op = nn.ModuleList()
 
         for i in range(self.num_subnets):
+            # We create each requested subnet
             self.subnets.append(SubUNet(num_classes, mean, std, depth=sub_net_depth))
+            # And for each pixel we output a weight for each subnet
             self.final_op.append(conv1x1(outs, self.num_classes))
 
 
@@ -296,26 +298,26 @@ class UNet(nn.Module):
             self.device), labels.to(self.device), masks.to(self.device)
         
         # Forward step
-        # [batch, subnets]
+        # Each pixel has its individual weight
+        # [batch, subnets, H, W]
         weights = self(inputs)
         # [subnets, batch, H, W]
         sub_outputs = [sub(inputs) for sub in self.subnets]
         # [batch, subnets, H, W]
         sub_outputs = np.transpose(sub_outputs, axes=(1, 0, 2, 3))
-
+        # [batch, subnets, H, W] x [batch, subnets, H, W]
         outputs = np.sum(weights * sub_outputs, axis=1)
-        
         # TODO check if this actually works correctly
         # This works in pixel-wise and image-wise case
-        outputs /= np.sum(weights, axis=0)
+        outputs /= np.sum(weights, axis=1)
         
         return sub_outputs, weights, labels, masks, data_counter
 
     def predict(self, image, patch_size, overlap):
-        # pixel weighted average
-        weighted_average = np.zeros(image.shape)
+        # weights for each subnet per pixel
+        weights = np.zeros((self.num_subnets, image.shape))
         # sub_images = [subnets, H, W]
-        sub_images = np.zeros((self.subnets.shape[0], image.shape))
+        sub_images = np.zeros((self.num_subnets, image.shape))
         # We have to use tiling because of memory constraints on the GPU
         xmin = 0
         ymin = 0
@@ -326,12 +328,12 @@ class UNet(nn.Module):
             ovTop = 0
             while (ymin < image.shape[0]):
                 patch = image[ymin:ymax, xmin:xmax]
-                weights = self.predict_patch(patch)
+                # self.predict_patch(patch) returns [num_subnets, H, W]
+                # i.e. one weight for each pixel
+                weights[:, ymin:ymax, xmin:xmax][ovTop:, ovLeft:]  
+                    = self.predict_patch(patch)
                 sub_images[:, ymin:ymax, xmin:xmax][ovTop:, ovLeft:] 
                     = [subnet.predict_patch(patch) for subnet in self.subnets][:, ovTop:, ovLeft:] 
-                weighted_average[ymin:ymax, xmin:xmax][ovTop:, ovLeft:] = 
-                    np.sum(weights[ovTop:, ovLeft:] * predicted_images[ovTop:, ovLeft:], axis=0) / 
-                                            np.sum(weights, axis=0)
                 ymin = ymin-overlap+patch_size
                 ymax = ymin+patch_size
                 ovTop = overlap//2
@@ -341,7 +343,10 @@ class UNet(nn.Module):
             xmax = xmin+patch_size
             ovLeft = overlap//2
 
-        return weighted_average
+        # weights * sub_images = [num_subnets, H, W] x [num_subnets, H, W]
+        weighted_average = np.sum(weights * sub_images, axis=0) / np.sum(weights, axis=0)
+
+        return weighted_average, weights
 
     def predict_patch(self, patch):
         """Performs network prediction on a patch of an image using the
@@ -368,14 +373,13 @@ class UNet(nn.Module):
         samples = (output).permute(1, 0, 2, 3)
 
         # In contrast to probabilistic N2V we only have one sample
-        means = samples[0, ...]
+        weights = samples[0, ...]
 
         # Get data from GPU
-        means = means.cpu().detach().numpy()
+        weights = weights.cpu().detach().numpy()
 
         # Reshape to 2D images and remove padding
-        means.shape = (output.shape[2], output.shape[3])
+        weights.shape = (output.shape[2], output.shape[3])
 
-        # Denormalize
-        means = util.denormalize(means, self.mean, self.std)
-        return means
+        # No denormalizing here as we output the weights
+        return weights

src/models/average/weight_trainer.py
@@ -16,18 +16,20 @@ import trainer
 class Trainer(trainer.Trainer):
 
     def _load_network(self):
-        # Device gets automatically created in constructor
-        # We persist mean and std when saving the network
         if self.config['weight_mode'] == 'pixel':
             from . import pixel_weight_network
-            self.net = pixel_weight_network.UNet(self.config['NUM_CLASSES'], self.loader.mean(),
-                        self.loader.std(), depth=self.config['DEPTH'])
+            net_module = pixel_weight_network
         else if self.config['weight_mode'] == 'image':
             from . import image_weight_network
-            self.net = image_weight_network.UNet(self.config['NUM_CLASSES'], self.loader.mean(),
-                        self.loader.std(), depth=self.config['DEPTH'])
+            net_module = image_weight_network
         else:
             raise 'Invalid config value for \"weight_mode\".'
+
+        self.net = net_module.UNet(self.config['NUM_CLASSES'], self.loader.mean(),
+                    self.loader.std(), depth=self.config['DEPTH'],
+                    main_net_depth=config['MAIN_NET_DEPTH'],
+                    sub_net_depth=config['SUB_NET_DEPTH'],
+                    num_subnets=config['NUM_SUBNETS'])
                         
         #TODO load pre-trained weights of network, if available
 
@@ -54,11 +56,8 @@ class Trainer(trainer.Trainer):
         psnr = util.PSNR(gt, prediction, 255)
         self.writer.add_scalar('psnr', psnr, self.print_step)
 
-        # Ugly but it works
-        plt.imsave('pred.png', prediction)
-        pred = plt.imread('pred.png')
-        self.writer.add_image('pred', pred, self.print_step, dataformats='HWC')
-        os.remove('pred.png')
+        prediction = prediction.astype(np.uint8)
+        self.writer.add_image('pred', prediction, self.print_step, dataformats='HW')
 
         for name, param in self.net.named_parameters():
             self.writer.add_histogram(

src/models/probabilistic/probabilistic_trainer.py
@@ -51,10 +51,8 @@ class Trainer(trainer.Trainer):
         self.writer.add_image('pred', pred, self.print_step, dataformats='HWC')
         os.remove('pred.png')
 
-        plt.imsave('std.png', self.std)
-        pred = plt.imread('std.png')
-        self.writer.add_image('std', self.std, self.print_step, dataformats='HWC')
-        os.remove('std.png')
+        prediction = prediction.astype(np.uint8)
+        self.writer.add_image('pred', prediction, self.print_step, dataformats='HW')
 
         for name, param in self.net.named_parameters():
             self.writer.add_histogram(

src/models/probabilistic/subnetwork_trainer.py
@@ -42,16 +42,10 @@ class Trainer(trainer.Trainer):
         psnr = util.PSNR(gt, prediction, 255)
         self.writer.add_scalar('psnr', psnr, self.print_step)
 
-        # So ugly but it works
-        plt.imsave('pred.png', prediction)
-        pred = plt.imread('pred.png')
-        self.writer.add_image('pred', pred, self.print_step, dataformats='HWC')
-        os.remove('pred.png')
+        prediction = prediction.astype(np.uint8)
+        self.writer.add_image('pred', prediction, self.print_step, dataformats='HW')
 
-        plt.imsave('std.png', self.std)
-        pred = plt.imread('std.png')
-        self.writer.add_image('std', self.std, self.print_step, dataformats='HWC')
-        os.remove('std.png')
+        self.writer.add_image('std', self.std.astype(np.uint8), self.print_step, dataformats='HW')
 
         for name, param in self.net.named_parameters():
             self.writer.add_histogram(