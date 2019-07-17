Started to correct implementation of average network.

src/models/average/trainer.py
@@ -7,18 +7,28 @@ import os
 import matplotlib.pyplot as plt
 import importlib
 
-from . import network
 import util
-import trainer
+import abstract_trainer
+from . import weight_network
 
-class Trainer(trainer.Trainer):
+class Trainer(abstract_trainer.AbstractTrainer):
 
     def _load_network(self):
-        # Device gets automatically created in constructor
-        # We persist mean and std when saving the network
-        self.net = network.UNet(self.config['NUM_CLASSES'], self.loader.mean(),
-                        self.loader.std(), depth=self.config['DEPTH'])
-        #TODO load pre-trained weights of network
+        if self.config['WEIGHT_MODE'] == 'image':
+            Network = weight_network.ImageWeightUNet
+        elif self.config['WEIGHT_MODE'] == 'pixel':
+            Network = weight_network.PixelWeightUNet
+        else:
+            raise 'Invalid config value for \"weight_mode\".'
+
+        self.net = Network(self.config['NUM_CLASSES'], self.loader.mean(),
+                    self.loader.std(), depth=self.config['DEPTH'],
+                    main_net_depth=self.config['MAIN_NET_DEPTH'],
+                    sub_net_depth=self.config['SUB_NET_DEPTH'],
+                    num_subnets=self.config['NUM_SUBNETS'],
+                    augment_data=self.config['AUGMENT_DATA'])
+                        
+        #TODO load pre-trained weights of network, if available
 
     def _create_checkpoint(self):
         return {'model_state_dict': self.net.state_dict(),
@@ -43,22 +53,20 @@ class Trainer(trainer.Trainer):
         psnr = util.PSNR(gt, prediction, 255)
         self.writer.add_scalar('psnr', psnr, self.print_step)
 
-        # Ugly but it works
-        plt.imsave('pred.png', prediction)
-        pred = plt.imread('pred.png')
-        self.writer.add_image('pred', pred, self.print_step, dataformats='HWC')
-        os.remove('pred.png')
+        prediction = prediction.astype(np.uint8)
+        self.writer.add_image('pred', prediction, self.print_step, dataformats='HW')
 
         for name, param in self.net.named_parameters():
             self.writer.add_histogram(
                 name, param.clone().cpu().data.numpy(), self.print_step)
 
     def _train(self):
-        outputs, labels, masks, self.dataCounter = self.net.training_predict(
-            self.data_train, self.data_train_gt, self.dataCounter, 
-            self.size, self.box_size, self.bs)
+        sub_outputs, weights, labels, masks, self.dataCounter =\
+            self.net.training_predict(
+                self.data_train, self.data_train_gt, self.dataCounter, 
+                self.size, self.box_size, self.bs)
 
-        self.train_loss = self.net.loss_function(outputs, labels, masks)
+        self.train_loss = self.net.loss_function(sub_outputs, weights, labels, masks)
         self.train_loss.backward()
         self.running_loss += self.train_loss.item()
         self.train_losses.append(self.train_loss.item())

src/models/average/weight_network.py
@@ -8,7 +8,7 @@ import numpy as np
 
 import util
 import abstract_network
-from subnetwork import SubUNet
+from .subnetwork import SubUNet
 
 class ImageWeightUNet(abstract_network.AbstractUNet):
     """ `UNet` class is based on https://arxiv.org/abs/1505.04597
@@ -51,11 +51,11 @@ class ImageWeightUNet(abstract_network.AbstractUNet):
                 for transpose convolution or 'upsample' for nearest neighbour
                 upsampling.
         """
-        super(ImageWeightUNet, self).__init__(num_classes, mean, std, in_channels, 
-                main_net_depth, start_filts, up_mode, merge_mode, augment_data, device)
-
         self.num_subnets = num_subnets
         self.sub_net_depth = sub_net_depth
+        super().__init__(num_classes, mean, std, in_channels, depth=main_net_depth, 
+                start_filts=start_filts, up_mode=up_mode, merge_mode=merge_mode, 
+                augment_data=augment_data, device=device)
 
     def _build_network_head(self, outs):
         self.subnets = nn.ModuleList()
@@ -63,7 +63,8 @@ class ImageWeightUNet(abstract_network.AbstractUNet):
 
         for i in range(self.num_subnets):
             # We create each requested subnet
-            self.subnets.append(SubUNet(num_classes, mean, std, depth=sub_net_depth))
+            self.subnets.append(SubUNet(self.num_classes, self.mean, self.std, 
+                                                    depth=self.sub_net_depth))
             # And we output a weight for each subnet based on the whole image
             # That's why we employ torch.sum
             self.final_op.append(torch.sum(conv1x1(outs, self.num_classes)))
@@ -155,7 +156,7 @@ class ImageWeightUNet(abstract_network.AbstractUNet):
                 patch = image[ymin:ymax, xmin:xmax]
                 # We save all weights for all patches and average later
                 weights.append(self.predict_patch(patch))
-                sub_images[:, ymin:ymax, xmin:xmax][ovTop:, ovLeft:] 
+                sub_images[:, ymin:ymax, xmin:xmax][ovTop:, ovLeft:]\
                     = [subnet.predict_patch(patch) for subnet in self.subnets][:, ovTop:, ovLeft:] 
                 ymin = ymin-overlap+patch_size
                 ymax = ymin+patch_size
@@ -234,6 +235,7 @@ class PixelWeightUNet(nn.Module):
     def __init__(self, num_classes, mean, std, in_channels=1, depth=5,
                  main_net_depth=1, sub_net_depth=3, num_subnets=2,
                  start_filts=64, up_mode='transpose', merge_mode='add',
+                 augment_data=True,
                  device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
         """
         NOTE: mean and std will be persisted by the model and restored on loading
@@ -250,11 +252,11 @@ class PixelWeightUNet(nn.Module):
                 for transpose convolution or 'upsample' for nearest neighbour
                 upsampling.
         """
-        super(PixelWeightUNet, self).__init__(num_classes, mean, std, in_channels, 
-                main_net_depth, start_filts, up_mode, merge_mode, augment_data, device)
-
         self.num_subnets = num_subnets
         self.sub_net_depth = sub_net_depth
+        super().__init__(num_classes, mean, std, in_channels, depth=main_net_depth, 
+                start_filts=start_filts, up_mode=up_mode, merge_mode=merge_mode, 
+                augment_data=augment_data, device=device)
 
     def _build_network_head(self, outs):
         self.subnets = nn.ModuleList()
@@ -262,7 +264,8 @@ class PixelWeightUNet(nn.Module):
 
         for i in range(self.num_subnets):
             # We create each requested subnet
-            self.subnets.append(SubUNet(num_classes, mean, std, depth=sub_net_depth))
+            self.subnets.append(SubUNet(self.num_classes, self.mean, self.std, 
+                                                    depth=self.sub_net_depth))
             # And for each pixel we output a weight for each subnet
             self.final_op.append(conv1x1(outs, self.num_classes))
 
@@ -357,9 +360,9 @@ class PixelWeightUNet(nn.Module):
                 patch = image[ymin:ymax, xmin:xmax]
                 # self.predict_patch(patch) returns [num_subnets, H, W]
                 # i.e. one weight for each pixel
-                weights[:, ymin:ymax, xmin:xmax][ovTop:, ovLeft:]  
+                weights[:, ymin:ymax, xmin:xmax][ovTop:, ovLeft:]\
                     = self.predict_patch(patch)
-                sub_images[:, ymin:ymax, xmin:xmax][ovTop:, ovLeft:] 
+                sub_images[:, ymin:ymax, xmin:xmax][ovTop:, ovLeft:]\
                     = [subnet.predict_patch(patch) for subnet in self.subnets][:, ovTop:, ovLeft:] 
                 ymin = ymin-overlap+patch_size
                 ymax = ymin+patch_size

src/models/probabilistic/probabilistic_network.py
@@ -58,9 +58,10 @@ class ImageProbabilisticUNet(abstract_network.AbstractUNet):
                 for transpose convolution or 'upsample' for nearest neighbour
                 upsampling.
         """
-        super(ImageProbabilisticUNet, self).__init__(num_classes, mean, std, 
-                in_channels, main_net_depth, start_filts, up_mode,
-                merge_mode, augment_data, device)
+        super().__init__(num_classes=num_classes, mean=mean, std=std, 
+                in_channels=in_channels, main_net_depth=main_net_depth, 
+                start_filts=start_filts, up_mode=up_mode, merge_mode=merge_mode, 
+                augment_data=augment_data, device=device)
 
         self.sub_net_depth = sub_net_depth
         self.num_subnets = num_subnets
@@ -239,9 +240,10 @@ class PixelProbabilisticUNet(nn.Module):
                 for transpose convolution or 'upsample' for nearest neighbour
                 upsampling.
         """
-        super(PixelProbabilisticUNet, self).__init__(num_classes, mean, std, 
-                in_channels, main_net_depth, start_filts, up_mode,
-                merge_mode, augment_data, device)
+        super().__init__(num_classes=num_classes, mean=mean, std=std, 
+                in_channels=in_channels, main_net_depth=main_net_depth, 
+                start_filts=start_filts, up_mode=up_mode, merge_mode=merge_mode, 
+                augment_data=augment_data, device=device)
 
         self.sub_net_depth = sub_net_depth
         self.num_subnets = num_subnets