Started implementing multi dataset tests. There is still a bug in the validation loss computation during training.

src/data/training_dataset.py
@@ -61,7 +61,7 @@ class TrainingDataset(Dataset):
 
     @staticmethod
     def _train_indices_permutation(indices):
-        return np.random.permutation(indices)
+        return np.random.permutation(indices).tolist()
 
     _val_indices_permutation = _train_indices_permutation
 
@@ -69,7 +69,7 @@ class TrainingDataset(Dataset):
 
     @staticmethod
     def _dataset_index_proportional(dataset_sizes):
-        probabilities = dataset_sizes / np.linalg.norm(dataset_sizes)
+        probabilities = dataset_sizes / np.sum(dataset_sizes)
         return np.random.choice(len(dataset_sizes), 1, p=probabilities)[0]
 
     def __init__(self, raw_images_dirs, gt_images_dirs=None, batch_size=24,
@@ -118,14 +118,18 @@ class TrainingDataset(Dataset):
 
         self._load_datasets(seed)
 
-        raws_size = len(np.array(self.raw_image_paths).flatten())
+        flattend_raw_image_paths = [image_path for sublist in\
+                                self.raw_image_paths for image_path in sublist]
+        raws_size = len(flattend_raw_image_paths)
         if self._train_mode == 'void':
             print('Using {} raw images and no gt images for training.'
                    .format(raws_size))
         else:
+            flattend_gt_image_paths = [image_path for sublist in\
+                                self.gt_image_paths for image_path in sublist]
             print('Using {} raw and {} gt images for training.'
                   .format(raws_size,
-                          len(np.array(self.gt_image_paths).flatten())))
+                          len(flattend_gt_image_paths)))
 
         self.mean, self.std = self._compute_mean_and_std()
         print('Dataset has mean {} and standard deviation {}.'\
@@ -315,7 +319,9 @@ class TrainingDataset(Dataset):
             std += tmp / float(self._raw_images.shape[0] * image.shape[0] * image.shape[1] - 1)
         """
         if self._keep_in_memory:
-            return np.mean(self.raw_images), np.std(self.raw_images)
+            # TODO not a nice solution since we store all images again
+            raws_flattened = [raw for sub in self.raw_images for raw in sub]
+            return np.mean(raws_flattened), np.std(raws_flattened)
         else:
             images = []
             for raw_image_paths in self.raw_image_paths:
@@ -323,7 +329,11 @@ class TrainingDataset(Dataset):
                     image = tif.imread(raw_image_path)
                     images.append(image)
 
-            mean, std = np.mean(images), np.std(images)
+            try:
+                mean, std = np.mean(images), np.std(images)
+            except ValueError:
+                raise ValueError('Are you using images of different shapes?')
+
             # Make sure the images get deleted right away
             del images
             return mean, std
@@ -504,7 +514,8 @@ class TrainingDataset(Dataset):
         Returns:
             int -- the length of this dataset
         """
-        return len(np.array(self.train_indices).flatten())
+        flattend_indices = [index for sublist in self.train_indices for index in sublist]
+        return len(flattend_indices)
 
     def __getitem__(self, idx):
         """Returns the image of the specified index. The Dataset class
@@ -575,10 +586,6 @@ class TrainingDataset(Dataset):
 
     def validation_samples(self):
         samples = []
-        if self._last_op_is_to_tensor:
-            module = torch
-        else:
-            module = np
         for dataset_index, val_indices in enumerate(self.val_indices):
             for val_index in val_indices:
                 if self._keep_in_memory:
@@ -589,17 +596,21 @@ class TrainingDataset(Dataset):
                     raw_image = tif.imread(self.raw_image_paths[dataset_index][val_index])
                     gt_image = tif.imread(self.gt_image_paths[dataset_index]\
                                     [int(val_index / self._factors[dataset_index])])
-                mask = module.stack([module.ones(raw_image.shape)])
-                if self._last_op_is_to_tensor:
-                    raw_image = util.img_to_tensor(raw_image)
-                    gt_image = util.img_to_tensor(gt_image)
-                raw_image = module.stack([raw_image])
-                gt_image = module.stack([gt_image])
                 sample = {'raw' : raw_image,
-                          'gt'  : gt_image,
-                          'mask': mask}
+                          'gt'  : gt_image}
                 if self.eval_transforms is not None:
                     sample = self.eval_transforms(sample)
+                raw_shape = sample['raw'].shape
+                # We have [batch_size, channels, H, W] as shape for raw images
+                # but only need [batch_size, H, W] for the mask
+                mask_shape = raw_shape[2:]
+                if self._last_op_is_to_tensor:
+                    # Add batch dimension
+                    sample['raw'] = torch.stack([sample['raw']]).float()
+                    sample['gt'] = torch.stack([sample['gt']]).float()
+                    sample['mask'] = torch.ones((1, 1) + mask_shape).float()
+                else:
+                    sample['mask'] = np.ones(mask_shape)
                 samples.append(sample)
                             
         return samples

tests/test_training_dataset.py
@@ -52,17 +52,22 @@ def _dataset_index_proportional_single_dataset(num_indices):
 
 def _dataset_index_proportional_multi_dataset_wihtout_val(num_indices):
     global CURRENT_MULTI_DATASET
+    print('curr', CURRENT_MULTI_DATASET)
     if CURRENT_MULTI_DATASET < 4:
-        return 0
+        result = 0
     elif CURRENT_MULTI_DATASET < 6:
         # now all indices of first dataset have been used up and refilled, i.e.
         # we need to continued at len(dataset_1)
-        return 4
+        result = 1
     elif CURRENT_MULTI_DATASET < 9:
         # now all indices of the second dataset have been used up and refilled,
         # i.e. we need to continue at len(dataset_1) + len(dataset_2)
-        return 6
-    raise ValueError
+        result = 2
+    else:
+        result = 0
+    CURRENT_MULTI_DATASET += 1
+    print('result', result)
+    return result
 
 @pytest.fixture(scope='module', autouse=True)
 def setup_module():
@@ -107,7 +112,7 @@ def abstract_test_single_dataset_iteration_raw_only_with_val(in_memory):
     dataset = TrainingDataset([conftest.dataset_1_raw_dir.name],
                               val_ratio=0.5, add_normalization_transform=False,
                               keep_in_memory=in_memory)
-    TrainingDataset._index_proportional = _dataset_index_proportional_single_dataset
+    TrainingDataset._dataset_index_proportional = _dataset_index_proportional_single_dataset
     assert len(dataset) == 2
     # indices[0] because we only have one dataset
     assert len(dataset.val_indices[0]) == 2
@@ -135,7 +140,7 @@ def abstract_test_single_dataset_raw_only_with_val(in_memory):
     dataset = TrainingDataset([conftest.dataset_1_raw_dir.name], batch_size=2,
                               val_ratio=0.5, add_normalization_transform=False,
                               keep_in_memory=in_memory)
-    TrainingDataset._index_proportional = _dataset_index_proportional_single_dataset
+    TrainingDataset._dataset_index_proportional = _dataset_index_proportional_single_dataset
     assert len(dataset) == 2
     # indices[0] because we only have one dataset
     assert len(dataset.val_indices[0]) == 2
@@ -170,7 +175,7 @@ def abstract_test_single_dataset_raw_only(in_memory):
     dataset = TrainingDataset([conftest.dataset_1_raw_dir.name], batch_size=4,
                               val_ratio=0, add_normalization_transform=False,
                               keep_in_memory=in_memory)
-    TrainingDataset._index_proportional = _dataset_index_proportional_single_dataset
+    TrainingDataset._dataset_index_proportional = _dataset_index_proportional_single_dataset
     # indices[0] because we only have one dataset
     assert len(dataset) == 4
     assert len(dataset.val_indices[0]) == 0
@@ -196,7 +201,7 @@ def abstract_test_single_dataset_raw_only_batch_size(in_memory):
     dataset = TrainingDataset([conftest.dataset_1_raw_dir.name], batch_size=16,
                               val_ratio=0, add_normalization_transform=False,
                               keep_in_memory=in_memory)
-    TrainingDataset._index_proportional = _dataset_index_proportional_single_dataset
+    TrainingDataset._dataset_index_proportional = _dataset_index_proportional_single_dataset
     assert len(dataset) == 4
     # indices[0] because we only have one dataset
     assert len(dataset.val_indices[0]) == 0
@@ -251,7 +256,7 @@ def abstract_test_single_dataset_raw_gt_with_val(in_memory):
                               batch_size=2, val_ratio=0.5,
                               add_normalization_transform=False,
                               keep_in_memory=in_memory)
-    TrainingDataset._index_proportional = _dataset_index_proportional_single_dataset
+    TrainingDataset._dataset_index_proportional = _dataset_index_proportional_single_dataset
     assert len(dataset) == 2
     # indices[0] because we only have one dataset
     assert len(dataset.val_indices[0]) == 2
@@ -317,3 +322,43 @@ def test_single_dataset_raw_only_with_val_even_on_demand():
 ##################################
 ############# Multi dataset tests
 ##################################
+
+def abstract_test_multi_dataset_raw_only(in_memory):
+    # Dataset 1 has 4 raw images and 2 gt images
+    dataset_1_raw = conftest.dataset_1_raw_images()
+    dataset_2_raw = conftest.dataset_2_raw_images()
+    dataset_3_raw = conftest.dataset_3_raw_images()
+    datasets = [dataset_1_raw, dataset_2_raw, dataset_3_raw]
+    dataset = TrainingDataset([conftest.dataset_1_raw_dir.name,
+                               conftest.dataset_2_raw_dir.name,
+                               conftest.dataset_3_raw_dir.name],
+                              batch_size=9, val_ratio=0,
+                              add_normalization_transform=False,
+                              keep_in_memory=in_memory)
+    TrainingDataset._dataset_index_proportional = _dataset_index_proportional_multi_dataset_wihtout_val
+    # indices[0] because we only have one dataset
+    assert len(dataset) == 9
+    assert len(np.array(dataset.val_indices).flatten()) == 0
+    # We get back a batch of size 4 with all images in order
+    sample = next(iter(dataset))
+    for i, raw in enumerate(sample['raw']):
+        mask = ~sample['mask'][i].astype(np.bool)
+        if i < 4:
+            idx = 0
+        elif i < 6:
+            idx = 1
+        else:
+            idx = 2
+        # Second half are training indices in TrainingDataset
+        dataset = datasets[idx]
+        assert np.array_equal(raw[mask], dataset[i][mask])
+        assert not np.array_equal(raw, dataset[i])
+        assert np.array_equal(sample['gt'][i], dataset[i])
+
+def test_multi_dataset_raw_only_in_memory():
+    #abstract_test_multi_dataset_raw_only(True)
+    pass
+
+def test_multi_dataset_raw_only_on_demand():
+    #abstract_test_multi_dataset_raw_only(False)
+    pass