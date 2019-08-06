4f3fe9bfdc8110c5c4bf2da49800f8017bc56748
Florian Blume <flob.91@gmx.de>
2019-08-06 12:26:06 +0200

    Fixed tests for prediction dataset.

---
+++ src/data/prediction_dataset.py
@@ -0,0 +1,85 @@
+import os
+import glob
+import numpy as np
+import tifffile as tif
+import natsort
+from torch.utils.data import Dataset
+from torchvision.transforms import Compose
+
+class PredictionDataset(Dataset):
+
+    def __init__(self, raw_images_dirs: list, gt_images_dirs=None,
+                 transform=None):
+        self._factors = []
+        self._indices = []
+        self._raw_images_dirs = raw_images_dirs
+        self._raw_images = []
+        with_ground_truth = gt_images_dirs is not None
+        if with_ground_truth:
+            self._gt_images = []
+            self._gt_images_dirs = gt_images_dirs       
+
+        self._test_mode = None
+        for i, raw_images_dir in enumerate(raw_images_dirs):
+            assert os.path.exists(raw_images_dir)
+            assert os.path.isdir(raw_images_dir)
+            raw_images = glob.glob(os.path.join(raw_images_dir, "*.tif"))
+            # Sort the unsorted files
+            raw_images = natsort.natsorted(raw_images)
+            self._raw_images.append(raw_images)
+            # For faster retrieval of the corresponding dataset
+            indices = [(i, im_index) for im_index in range(len(raw_images))]
+            self._indices.extend(indices)
+
+            # If there are no ground-truth images we learn the network
+            # Noise2Void style, otherwise we train it Noise2Clean
+            if with_ground_truth:
+                if self._test_mode is not None and self._test_mode == 'void':
+                    raise ValueError('Cannot predict clean and void style '+
+                                     ' at the same time.')
+                gt_images_dir = gt_images_dirs[i]
+                assert os.path.exists(gt_images_dir)
+                assert os.path.isdir(gt_images_dir)
+                gt_images = glob.glob(os.path.join(gt_images_dir, "*.tif"))
+                # Sort the unsorted files
+                gt_images = natsort.natsorted(gt_images)
+                self._gt_images.append(gt_images)
+                # Same number of raw and ground-truth images
+                factor = len(raw_images) / len(gt_images)
+                assert factor.is_integer(), 'Number of raw images needs to be '+\
+                                            'divisible by the number of ground-'+\
+                                            'truth images.'
+                self._factors.append(factor)
+                self._test_mode = 'clean'
+            else:
+                if self._test_mode is not None and self._test_mode == 'clean':
+                    raise ValueError('Cannot predict clean and void style '+
+                                     ' at the same time.')
+                # If we want to train N2V style
+                self._test_mode = 'void'
+
+        self._raw_images = np.array(self._raw_images)
+        if gt_images_dirs is not None:
+            self._gt_images = np.array(self._gt_images)
+        if transform is not None:
+            self._transform = Compose(transform)
+        else:
+            self._transform = None
+
+    def __len__(self):
+        return len(self._indices)
+
+    def __getitem__(self, idx):
+        dataset, im_index = self._indices[idx][0], self._indices[idx][1]
+        raw_image = tif.imread(self._raw_images[dataset][im_index])
+        if self._test_mode == 'clean':
+            gt_image = tif.imread(self._gt_images[dataset]\
+                                       [int(im_index / self._factors[dataset])])
+            sample = {'raw' : raw_image, 'gt' : gt_image}
+        else:
+            sample = {'raw' : raw_image}
+
+        if self._transform is not None:
+            sample = self._transform(sample)
+
+        return sample
--- tests/test_prediction_dataset.py
+++ tests/test_prediction_dataset.py
@@ -1,57 +1,77 @@
-import shutil
-import tempfile
-import pytest
 import numpy as np
-import tifffile as tif
 
 from tests import base_test
 from tests import conftest
 
 from data import PredictionDataset
 
-def test_raw_images():
-    """This test case performs a simple test without transformations and checks
-    whether raw images are returned correctly when specifing
-    a validation ratio of 0.3 and Noise2Void training.
-    """
-    dataset = PredictionDataset([conftest.tmp_raw_dir.name])
-    assert len(dataset) == 3
-    raw_images = np.array(conftest.create_raw_images())
-    for i in range(len(dataset)):
-        dataset_image = dataset[i]
+def test_single_dataset_raw_images():
+    dataset = PredictionDataset([conftest.dataset_1_raw_dir.name])
+    assert len(dataset) == 4
+    raw_images = np.array(conftest.dataset_1_raw_images())
+    for i, dataset_image in enumerate(dataset):
         raw_image = dataset_image['raw']
         assert np.array_equal(raw_image, raw_images[i])
 
-def test_gt_images():
-    """This test case performs a simple test without transformations and checks
-    whether raw images are returned correctly when specifing
-    a validation ratio of 0.3 and Noise2Void training.
-    """
-    dataset = PredictionDataset([conftest.tmp_raw_dir.name], 
-                                [conftest.tmp_gt_dir.name])
-    assert len(dataset) == 3
-    raw_images = np.array(conftest.create_raw_images())
-    gt_images = np.array(conftest.create_gt_images())
-    for i in range(len(dataset)):
-        dataset_image = dataset[i]
+def test_single_dataset_gt_images():
+    dataset = PredictionDataset([conftest.dataset_1_raw_dir.name],
+                                [conftest.dataset_1_gt_dir.name])
+    assert len(dataset) == 4
+    raw_images = np.array(conftest.dataset_1_raw_images())
+    gt_images = np.array(conftest.dataset_1_gt_images())
+    factor = int(len(raw_images) / len(gt_images))
+    for i, dataset_image in enumerate(dataset):
         raw_image = dataset_image['raw']
         gt_image = dataset_image['gt']
         assert np.array_equal(raw_image, raw_images[i])
-        assert np.array_equal(gt_image, gt_images[i])
+        assert np.array_equal(gt_image, gt_images[int(i / factor)])
 
-def test_multi_path():
-    dataset = PredictionDataset([conftest.tmp_raw_dir.name, conftest.tmp_raw_dir_2.name],
-                                [conftest.tmp_gt_dir.name, conftest.tmp_gt_dir_2.name])
-    raw = conftest.create_raw_images()
-    raw2 = conftest.create_raw_images_2()
+def test_multi_dataset_raw_images():
+    dataset = PredictionDataset([conftest.dataset_1_raw_dir.name, 
+                                 conftest.dataset_2_raw_dir.name])
+    raw = conftest.dataset_1_raw_images()
+    raw2 = conftest.dataset_2_raw_images()
     raw.extend(raw2)
     raw = np.array(raw)
-    gt = conftest.create_gt_images()
-    gt2 = conftest.create_gt_images_2()
-    gt.extend(gt2)
-    gt = np.array(gt)
     for i, sample in enumerate(dataset):
+        raw_ = sample['raw']
+        assert np.array_equal(raw[i], raw_)
+
+def test_multi_dataset_gt_images():
+    dataset = PredictionDataset([conftest.dataset_1_raw_dir.name,
+                                 conftest.dataset_2_raw_dir.name,
+                                 conftest.dataset_3_raw_dir.name],
+                                [conftest.dataset_1_gt_dir.name,
+                                 conftest.dataset_2_gt_dir.name,
+                                 conftest.dataset_3_gt_dir.name])
+    raw = conftest.dataset_1_raw_images()
+    raw2 = conftest.dataset_2_raw_images()
+    raw3 = conftest.dataset_3_raw_images()
+    gt = conftest.dataset_1_gt_images()
+    gt2 = conftest.dataset_2_gt_images()
+    gt3 = conftest.dataset_3_gt_images()
+    factors = [int(len(raw) / len(gt)),
+               int(len(raw2) / len(gt2)),
+               int(len(raw3) / len(gt3))]
+    raw.extend(raw2)
+    raw.extend(raw3)
+    raw = np.array(raw)
+    for i, sample in enumerate(dataset):
+        if i < 4:
+            factor = factors[0]
+            current_gt = gt
+            gt_idx = i
+        elif i < 6:
+            factor = factors[1]
+            current_gt = gt2
+            # because first dataset has 4 entries
+            gt_idx = i - 4
+        else:
+            factor = factors[2]
+            current_gt = gt3
+            # because second dataset has an additional 2 entries
+            gt_idx = i - 6
         raw_ = sample['raw']
         gt_ = sample['gt']
         assert np.array_equal(raw[i], raw_)
-        assert np.array_equal(gt[i], gt_)
+        assert np.array_equal(current_gt[int(gt_idx / factor)], gt_)
