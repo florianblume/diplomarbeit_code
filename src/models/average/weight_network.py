Fixed another bug in average network. Adjusted batch size of fish and simsim to avoid CUDA memory errors.

src/models/average/weight_network.py
@@ -59,13 +59,15 @@ class AbstractWeightNetwork(AbstractUNet):
         if self.weight_mode == 'image':
             # Add dimension s.t. summed up weights can be broadcasted
             weights_sum = weights_sum.unsqueeze(-1)
+            sum_dims = 0
         else:
             # weight mode 'pixel'
             weights_sum = weights_sum.unsqueeze(1)
+            sum_dims = (0, 2, 3)
         weights = weights / weights_sum
-        # Mean along dimension (0, 2, 3) to obtain the mean of the weights
+        # Mean along dimension 0 to obtain the mean of the weights
         # along batch and H and W
-        weights = torch.mean(weights, (0, 2, 3))
+        weights = torch.mean(weights, sum_dims)
         entropy = -torch.sum(weights * torch.log(weights))
         return loss - self.weight_constraint_lambda * entropy
 

src/models/probabilistic/probabilistic_network.py
@@ -74,7 +74,10 @@ class ImageProbabilisticUNet(AbstractUNet):
         # One probability (weight) for each subnet
         if self.num_subnets == 2:
             # probabilities are of shape [batch, subnet, H, W] before taking mean
-            probabilities = torch.mean(self.weight_probabilities(x), (-2, -1))
+            probabilities = self.weight_probabilities(x)
+            print(probabilities.shape)
+            probabilities = torch.mean(probabilities, (-2, -1))
+            print('s', probabilities.shape)
             first_prob = torch.sigmoid(probabilities)
             outs = torch.stack([first_prob, 1 - first_prob])
         else: