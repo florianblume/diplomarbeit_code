Fixed averaging network implementation after changing code to output 2 channels for std and mean instead of having two separate branches.

src/models/abstract_network.py
@@ -289,8 +289,9 @@ class AbstractUNet(nn.Module):
         while (xmin < image_width):
             ovTop = 0
             while (ymin < image_height):
-                data = self._process_patch(data, ymin, ymax, xmin, 
-                                           xmax, ovTop, ovLeft)
+                # We do not receive anything from this method because it
+                # modifies the data in-place
+                self._process_patch(data, ymin, ymax, xmin, xmax, ovTop, ovLeft)
                 ymin = ymin-self.overlap+self.patch_size
                 ymax = ymin+self.patch_size
                 ovTop = self.overlap//2
@@ -314,6 +315,8 @@ class AbstractUNet(nn.Module):
         """Performs prediction on the specified patch coordinates given the
         specified data dictionary. This method is for internal use of the
         prediction pipeline only.
+
+        NOTE: Does not return anything because it modifies the data in-place.
         
         Arguments:
             data {dict} -- a dictionary with the data to work on

src/models/probabilistic/probabilistic_network.py
@@ -71,12 +71,13 @@ class ImageProbabilisticUNet(AbstractUNet):
         sub_probs = [log_probs[i] * sub_losses[i] * -1.0 for i in self.num_subnets]
         # Due to the log-exp trick we can subtract the maximum. This means we
         # obtain smaller numbers and are less prone to numerical issues.
-        max_prob = torch.max(sub_probs)
-        sub_probs -= max_prob
+        sub_probs -= torch.max(sub_probs)
         sub_probs = F.softmax(sub_probs, dim=0)
         # p(z|x,y) is multiplied to the gradient as a factor and must not
         # receive a gradient during backpropagation
         sub_probs = sub_probs.detach()
+
+        return log_probs * sub_probs + sub_losses * sub_probs
 
     def forward(self, x):
         encoder_outs = []
@@ -108,57 +109,41 @@ class ImageProbabilisticUNet(AbstractUNet):
 
         # Forward step
         weights = self(raw)
-        means, stds = [subnet(raw) for subnet in self.subnets]
+        sub_outputs = [subnet(raw) for subnet in self.subnets]
+        # Slice notation to keep dimensions
+        means = sub_outputs[:, :1]
+        stds = sub_outputs[:, 1:]
 
-        # TODO THIS IS NOT CORRECT!!!
-        # The correct formula is w_i = 1/sigma_i^2
-        # bar(x) = sum(x_i sigma_i^(-2))/sum(sigma_i^(-2)) (mean)
-        # sigma_bar(x) = sqrt(1 / sum(sigma_i^(-2)))
-        # See also https://en.wikipedia.org/wiki/Weighted_arithmetic_mean#Variance_weights
-        means_out = np.array([w * m for w in weights for m in means])
-        stds_out = np.array([w * m for w in weights for m in stds])
-        #TODO this might be wrong
-        means_out = np.sum(means_out, axis=0)
-        stds_out = np.sum(stds_out, axis=0)
+        # TODO the standard deviation is not simply the weighted average
+        # https://stats.stackexchange.com/questions/16608/what-is-the-variance-of-the-weighted-mixture-of-two-gaussians
+        means_out = torch.stack([w * m for w in weights for m in means])
+        stds_out = torch.stack([w * m for w in weights for m in stds])
+        
+        means_out = torch.sum(means_out, dim=0)
+        stds_out = torch.sum(stds_out, dim=0)
         return {'output' : means_out,
                 'mean'   : means_out,
                 'std'    : stds_out,
                 'gt'     : ground_truth,
                 'mask'   : mask}
 
-    def predict(self, image, patch_size, overlap):
+    def _pre_process_predict(self, image):
         mean = np.zeros(image.shape)
         std = np.zeros(image.shape)
-        
-        image_height = image.shape[-2]
-        image_width = image.shape[-1]
+        return {'image' : image,
+                'mean'  : mean,
+                'std'   : std}
 
-        xmin = 0
-        ymin = 0
-        xmax = patch_size
-        ymax = patch_size
-        ovLeft = 0
-        while (xmin < image_height):
-            ovTop = 0
-            while (ymin < image_width):
-                mean_, std_ = self.predict_patch(
-                                    image[:, :, ymin:ymax, xmin:xmax])
-                mean[:, :, ymin:ymax, xmin:xmax]\
-                        [:, :, ovTop:, ovLeft:] = mean_[:, :, ovTop:, ovLeft:]
-                std[:, :, ymin:ymax, xmin:xmax]\
-                        [:, :, ovTop:, ovLeft:] = std_[:, :, ovTop:, ovLeft:]
-                ymin = ymin-overlap+patch_size
-                ymax = ymin+patch_size
-                ovTop = overlap//2
-            ymin = 0
-            ymax = patch_size
-            xmin = xmin-overlap+patch_size
-            xmax = xmin+patch_size
-            ovLeft = overlap//2
-
-        #TODO assemble final image here
-
-        return mean, std
+    def _process_patch(self, data, ymin, ymax, xmin, xmax, ovTop, ovLeft):
+        image = data['image']
+        mean = data['mean']
+        std = data['std']
+        mean_, std_ = self.predict_patch(
+                            image[:, :, ymin:ymax, xmin:xmax])
+        mean[:, :, ymin:ymax, xmin:xmax]\
+                [:, :, ovTop:, ovLeft:] = mean_[:, :, ovTop:, ovLeft:]
+        std[:, :, ymin:ymax, xmin:xmax]\
+                [:, :, ovTop:, ovLeft:] = std_[:, :, ovTop:, ovLeft:]
 
     def predict_patch(self, patch):
         inputs = patch.to(self.device)
@@ -169,6 +154,10 @@ class ImageProbabilisticUNet(AbstractUNet):
         mean = util.denormalize(mean, self.mean, self.std)
         std = util.denormalize(std, self.mean, self.std)
         return mean, std
+
+    def _post_process_predict(self, result):
+        pass
+        #TODO assemble final image here
 
 class PixelProbabilisticUNet(AbstractUNet):
 

src/models/probabilistic/subnetwork.py
@@ -17,8 +17,8 @@ class SubUNet(AbstractUNet):
         super(SubUNet, self).__init__(config)
 
     def _build_network_head(self, outs):
-        # 2 output channels for mean and std, respectively
-        self.conv_final = conv1x1(outs, 2)
+        # Mean and std as for each input channel
+        self.conv_final = conv1x1(outs, self.in_channels * 2)
 
     def loss_function(self, result):
         if self.is_integrated:
@@ -68,7 +68,10 @@ class SubUNet(AbstractUNet):
             x = module(before_pool, x)
 
         output = self.conv_final(x)
-        mean, std = output[:, 0], output[:, 1]
+
+        # First entries are the means of the channels, the second are the stds
+        mean = output[:, 0:self.in_channels]
+        std = output[:, self.in_channels:2*self.in_channels]
         # exp makes std positive (which it always is)
         std = torch.exp(std)
         return mean, std
@@ -103,9 +106,6 @@ class SubUNet(AbstractUNet):
                                             _mean[:, :, ovTop:, ovLeft:]
         std[:, :, ymin:ymax, xmin:xmax][:, :, ovTop:, ovLeft:] =\
                                             _std[:, :, ovTop:, ovLeft:]
-        data['mean'] = mean
-        data['std'] = std
-        return data
 
     def predict_patch(self, patch):
         inputs = patch.to(self.device)
@@ -126,7 +126,7 @@ class SubUNet(AbstractUNet):
         std = result['std']
         # At the moment we always have implicit batch size 1
         out_image = mean[0]
-        # Transpose to [C, H, W]
+        # Transpose to [H, W, C]
         out_image = out_image.transpose(1, 2, 0)
         
         return {'output' : out_image,